16f40a796b86548e0eda375fcef39d2acaf50a83
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-08-09 14:31:35 -0300

    :tada: init

---
+++ bin/escribo.dart
@@ -0,0 +1,5 @@
+import 'package:escribo/escribo.dart' as escribo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${escribo.calculate()}!');
+}
---
+++ test/escribo_test.dart
@@ -0,0 +1,8 @@
+import 'package:escribo/escribo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
