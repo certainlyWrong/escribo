04a86f55af181f6a297ccac8e9621ba67998bcb0
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-08-09 15:13:24 -0300

    :white_check_mark: Update: tests

--- test/escribo_test.dart
+++ test/escribo_test.dart
@@ -1,8 +1,20 @@
-import 'package:escribo/escribo.dart';
+import 'package:escribo/escribo.lib.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Números négativos', () {
+    expect(calculate(-42), 0);
+  });
+
+  test('O valor 0 como entrada', () {
+    expect(calculate(0), 0);
+  });
+
+  test('O valor 10 como entrada', () {
+    expect(calculate(10), 23);
+  });
+
+  test('O valor 11 como entrada', () {
+    expect(calculate(11), 33);
   });
 }
