bd5ee8130094c111e1f5624709771337524adb13
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2023-08-03 20:43:40 -0300

    Remove: test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,14 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:teste_tecnico_02/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:escribo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const EscriboGame(tile: 16));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
